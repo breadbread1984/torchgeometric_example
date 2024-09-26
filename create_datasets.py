c88ab460e7f4daa2cc21b0fd2b44c2a5abab2a89
breadbread1984 <breadbread1984@163.com>
2024-09-26 02:38:28 +0000

    update

--- create_datasets.py
+++ create_datasets.py
@@ -34,7 +34,7 @@ class Molecule(Dataset):
     x = F.one_hot(torch.tensor(nodes, dtype = torch.long),118).to(torch.float32) # x.shape = (node_num, 118)
     edge_index = torch.tensor(edges, dtype = torch.long).t().contiguous() # edge_index.shape = (2, edge_num)
     edge_type = F.one_hot(torch.tensor(edges_type, dtype = torch.long),22).to(torch.float32) # edge_type.shape = (edge_num)
-    data = Data(x = x, edge_index = edge_index, edge_type = edge_type, y = torch.tensor(label, dtype = torch.float32))
+    data = Data(x = x, edge_index = edge_index, edge_type = edge_type, y = torch.tensor([label], dtype = torch.float32))
     return data
 
 if __name__ == "__main__":
